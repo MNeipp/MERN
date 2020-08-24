cbb1829059812bc0e0c93d0c05cd82cfa5bd6af9
MNeipp <ReamusLQ@gmail.com>
2020-08-24 17:41:42 -0600

    putting it together assignment

--- react/first-react-app/src/components/person-card.js
+++ react/first-react-app/src/components/person-card.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+    }
+}
+incrementAge = () =>{
+            this.setState(state => ({ age: state.age + 1 }))
+        }
+    
     render(){
-        const {firstName, lastName, age, hairColor} = this.props;
+        const {firstName, lastName, hairColor} = this.props;
         return(
             <> 
             <h1>{ lastName }, {firstName}</h1>
-            <h4>Age: {age}</h4>
+            <h4>Age: {this.state.age}</h4>
             <h4>Hair Color: {hairColor}</h4>
+            <button onClick={ this.incrementAge }>Birthday Button for {firstName} {lastName}</button>
             </> 
         )
     }
