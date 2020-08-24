b9aeb518884ecaafed2e8137a425c95b7ed4b38b
MNeipp <ReamusLQ@gmail.com>
2020-08-24 15:41:03 -0600

    First JSX assignment

--- react/first-react-app/src/App.js
+++ react/first-react-app/src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>I edited the App.js</p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello Dojo!</h1>
+    <h2>Things I Need to do:</h2>
+    <ul>
+      <li>Learn React</li>
+      <li>Climb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed Eevee</li>
+    </ul>
+    </>
   );
 }
 
