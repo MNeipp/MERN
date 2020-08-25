3981c82460a3b9768756582c8510a44604f21df6
MNeipp <ReamusLQ@gmail.com>
2020-08-25 14:08:50 -0600

    refactored assignment

--- react/first-react-app/src/components/person-card.js
+++ react/first-react-app/src/components/person-card.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
+
 const PersonCard = props =>{
+    const {lastName, firstName, age, hairColor} = props
     return(
         <> 
-        <h1>{ props.lastName }, {props.firstName}</h1>
-        <h4>Age: {props.age}</h4>
-        <h4>Hair Color: {props.hairColor}</h4>
-        <button>Birthday Button for {props.firstName} {props.lastName}</button>
+        <h1>{ lastName }, {firstName}</h1>
+        <h4>Age: {age}</h4>
+        <h4>Hair Color: {hairColor}</h4>
+        <button>Birthday Button for {firstName} {lastName}</button>
         </> 
         )
     }
