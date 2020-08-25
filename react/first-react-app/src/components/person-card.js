905fe78bf23a6d25595b6327c09141eb83e75859
MNeipp <ReamusLQ@gmail.com>
2020-08-25 14:04:27 -0600

    big inversion assignment

--- react/first-react-app/src/components/person-card.js
+++ react/first-react-app/src/components/person-card.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PersonCard extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            age: this.props.age
-    }
-}
-incrementAge = () =>{
-            this.setState(state => ({ age: state.age + 1 }))
-        }
-    
-    render(){
-        const {firstName, lastName, hairColor} = this.props;
-        return(
-            <> 
-            <h1>{ lastName }, {firstName}</h1>
-            <h4>Age: {this.state.age}</h4>
-            <h4>Hair Color: {hairColor}</h4>
-            <button onClick={ this.incrementAge }>Birthday Button for {firstName} {lastName}</button>
-            </> 
+const PersonCard = props =>{
+    return(
+        <> 
+        <h1>{ props.lastName }, {props.firstName}</h1>
+        <h4>Age: {props.age}</h4>
+        <h4>Hair Color: {props.hairColor}</h4>
+        <button>Birthday Button for {props.firstName} {props.lastName}</button>
+        </> 
         )
     }
-}
 
-export default PersonCard;
+export default PersonCard
